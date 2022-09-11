61fe017e2453507a1a6ee2cfd10f48794ed0169b
Yusuf Ridwan <yusufridwanayomide.com>
2022-09-11 12:22:51 +0100

    still working

--- .vscode/Day 2 Asabeneh/variable.js
+++ .vscode/Day 2 Asabeneh/variable.js
@@ -48,3 +48,9 @@ console.log(`${string3.substr(0,30)} ${string3.substr(55)}`);
 
 const sentence = '%I $am@% a %tea@cher%, &and& I lo%#ve %te@a@ching%;. The@re $is no@th@ing; &as& mo@re rewarding as educa@ting &and& @emp%o@weri@ng peo@ple. ;I found tea@ching m%o@re interesting tha@n any ot#her %jo@bs. %Do@es thi%s mo@tiv#ate yo@u to be a tea@cher!? %Th#is 30#Days&OfJavaScript &is al@so $the $resu@lt of &love& of tea&ching';
 console.log(sentence.replace(/@|%|\$|&|#/gi,""));
+
+let annualIncome='He earns 5000 euro from salary per month, 10000 euro annual bonus, 15000 euro online courses per month.'
+let annualamount=annualIncome.match(/\d+/g);
+let annualIncomeResult=(annualamount[0]*12)+(annualamount[1])+(annualamount[2]*12);
+console.log("The person total annual income is : "+annualIncomeResult);
+ 
