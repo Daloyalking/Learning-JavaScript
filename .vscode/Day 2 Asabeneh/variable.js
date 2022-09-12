88490288857159512403225acb877316288e27af
Yusuf Ridwan <yusufridwanayomide.com>
2022-09-12 20:03:04 +0100

    Still Working

--- .vscode/Day 2 Asabeneh/variable.js
+++ .vscode/Day 2 Asabeneh/variable.js
@@ -46,6 +46,12 @@ console.log(`Random number between 50 and 100 is: ${randomNumber1}`);
 let string3='You cannot end a sentence with because because because is a conjunction'
 console.log(`${string3.substr(0,30)} ${string3.substr(55)}`);
 
+//Access the 'JavaScript' string characters using a random number.
+let accessString='JavaScript';
+let randomResult=Math.floor(Math.random()*accessString.length);
+let javaIndex=accessString[randomResult];
+console.log("Result of going through JAvaScript "+javaIndex);
+
 const sentence = '%I $am@% a %tea@cher%, &and& I lo%#ve %te@a@ching%;. The@re $is no@th@ing; &as& mo@re rewarding as educa@ting &and& @emp%o@weri@ng peo@ple. ;I found tea@ching m%o@re interesting tha@n any ot#her %jo@bs. %Do@es thi%s mo@tiv#ate yo@u to be a tea@cher!? %Th#is 30#Days&OfJavaScript &is al@so $the $resu@lt of &love& of tea&ching';
 console.log(sentence.replace(/@|%|\$|&|#/gi,""));
 
